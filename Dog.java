49d03722a96d783db5184fc5f3dfddd4c61c5aa3
aaron cheese skibidi rizz <126525235+aaron21913@users.noreply.github.com>
2024-12-11 19:03:50 +0000

    lol

--- Dog.java
+++ Dog.java
@@ -3,4 +3,19 @@ public class Dog
     private string breed;
     private double size;
     private string colorOfFur;
+
+    public void setBreed(String iBreed);
+    {
+        breed = iBreed;
+    }
+
+    public void setColor(String iColor)
+    {
+        colorOfFur = iColor;
+    }
+
+    public void setSize(double iSize)
+    {
+        size = iSize;
+    }
 }
--- Main.java
+++ Main.java
@@ -8,17 +8,22 @@ public class Main
     Point2D p1 = new Point2D();
     Point2D p2 = new Point2D();
 
-    p1.setX(5);
-    p2.setX(10);
+    //p1.setX(5);
+    //p2.setX(10);
 
-    p1.setY(3);
-    p2.setY(5);
+    //p1.setY(3);
+    //p2.setY(5);
 
     // Ohio Dog
     Dog OhioDog = new Dog();
     OhioDog.breed = "daschund";
     OhioDog.size = 10.2;
     OhioDog.colorOfFur = "brown";
+
+    Dog RizzDog = new Dog();
+    RizzDog.setBreed("chihuahua");
+    RizzDog.setColor("Green");
+    RizzDog.setSize(19.6);
 
 
   }
