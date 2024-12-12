8bfa85380e857ea76685c7bae9525425ea889115
aaron cheese skibidi rizz <126525235+aaron21913@users.noreply.github.com>
2024-12-12 19:20:22 +0000

    toilet

--- Person.java
+++ Person.java
@@ -1,4 +1,37 @@
 public class Person
 {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int ssn;
 
+    public Person(String fName, String lName. int iAge, String iSsn)
+    {
+        firstName = fName;
+        lastName = lName;
+        age = iAge;
+        ssn = iSsn;
+    }
+
+    public String getFirstName() 
+    {
+        return firstName;
+    }
+
+    public String getLastName() 
+    {
+        return lastName;
+    }
+
+    public int getAge() 
+    {
+        return age;
+    }
+
+    public String getSsn() 
+    {
+        return ssn;
+    }
+
+    
 }
